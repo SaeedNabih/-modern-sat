c521da22e37b8f747b6d574a0ec51cd4d15983ce
SaeedNabih <saidtolan111@gmail.com>
2025-10-09 22:14:42 +0300

    fix: update next.config.js and add auto-save feature

--- next.config.js
+++ next.config.js
@@ -1,27 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // إعدادات الأمان
   async headers() {
     return [
       {
         source: "/(.*)",
         headers: [
-          {
-            key: "X-Robots-Tag",
-            value: "noindex, nofollow, noarchive, nocache",
-          },
-          {
-            key: "Cache-Control",
-            value: "no-cache, no-store, must-revalidate",
-          },
-          {
-            key: "Pragma",
-            value: "no-cache",
-          },
-          {
-            key: "Expires",
-            value: "0",
-          },
           {
             key: "X-Frame-Options",
             value: "DENY",
